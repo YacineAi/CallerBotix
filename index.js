0851190ea6cf1115588d3ecf0853ff8ee97b8b09
Yacine Djenidi <djenidi.dev@gmail.com>
2023-07-15 16:37:45 +0100

    Fixer

--- index.js
+++ index.js
@@ -154,8 +154,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/webhook", botly.router());
 
 botly.on("message", async (senderId, message) => {
-  botly.sendText({ id: senderId, text: "يجري إصلاح الصفحة الان :) تمهل قليلا...." });
-  /*
   const user = await userDb(senderId);
     const timer = new Date().getTime() + 24 * 60 * 60 * 1000;
     const time = new Date().getTime();
@@ -371,11 +369,9 @@ botly.on("message", async (senderId, message) => {
       });
     }
     //  e n d  //
-    */
 });
 
 botly.on("postback", async (senderId, message, postback) => {
-  /*
     // s t a r t  //
     const user = await userDb(senderId);
     const time = new Date().getTime();
@@ -803,7 +799,6 @@ botly.on("postback", async (senderId, message, postback) => {
             });
     }
     //   e n d   //
-    */
   });
   /*------------- RESP -------------*/
   /*
